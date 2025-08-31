acac38c88dfb3fa5e8bf0e13ed877b1382280789
JiHoon-0330 <ljihoon0330@gmail.com>
2025-08-31 17:57:24 +0900

    서버환경 msw 설정

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,5 +1,6 @@
 import fs from "node:fs/promises";
 import express from "express";
+import { mswServer } from "./src/mocks/node.js";
 
 const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
@@ -8,6 +9,10 @@ const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "/")
 const templateHtml = prod ? await fs.readFile("./dist/vanilla/index.html", "utf-8") : "";
 
 const app = express();
+
+mswServer.listen({
+  onUnhandledRequest: "bypass",
+});
 
 // Add Vite or respective production middlewares
 /** @type {import('vite').ViteDevServer | undefined} */
@@ -46,11 +51,12 @@ app.use("*all", async (req, res) => {
       render = (await import("./dist/vanilla-ssr/main-server.js")).render;
     }
 
-    const rendered = await render(url);
+    const rendered = await render(url, req.query);
 
     const html = template
       .replace(`<!--app-head-->`, rendered.head ?? "")
       .replace(`<!--app-html-->`, rendered.html ?? "");
+
     res.status(200).set({ "Content-Type": "text/html" }).send(html);
   } catch (e) {
     vite?.ssrFixStacktrace(e);
