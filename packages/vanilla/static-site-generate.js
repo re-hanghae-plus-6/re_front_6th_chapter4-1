9259b3da7b783d3fcaadd357548139093262d432
JiHoon-0330 <ljihoon0330@gmail.com>
2025-09-01 14:29:58 +0900

    ssg 설정

--- packages/vanilla/static-site-generate.js
+++ packages/vanilla/static-site-generate.js
@@ -1,20 +1,68 @@
-import fs from "fs";
+import fs from "node:fs";
+import items from "./src/mocks/items.json" with { type: "json" };
+import { createServer } from "vite";
+import path from "node:path";
+import { mswServer } from "./src/mocks/node.js";
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+mswServer.listen({
+  onUnhandledRequest: "bypass",
+});
 
-async function generateStaticSite() {
-  // HTML 템플릿 읽기
-  const template = fs.readFileSync("../../dist/vanilla/index.html", "utf-8");
+const vite = await createServer({
+  server: { middlewareMode: true },
+  appType: "custom",
+});
 
-  // 어플리케이션 렌더링하기
-  const appHtml = render();
+const { render } = await vite.ssrLoadModule("./src/main-server.js");
 
-  // 결과 HTML 생성하기
-  const result = template.replace("<!--app-html-->", appHtml);
-  fs.writeFileSync("../../dist/vanilla/index.html", result);
+const joinDist = (...pathnames) => path.join("../../dist/vanilla", ...pathnames);
+
+const template = fs.readFileSync(joinDist("/index.html"), "utf-8");
+
+async function generateStaticSite(pathname, ssg) {
+  const fullPathname = pathname.endsWith(".html") ? joinDist(pathname) : joinDist(pathname, "/index.html");
+  const parsedPath = path.parse(fullPathname);
+
+  const rendered = await render(pathname, {}, ssg);
+
+  const html = template
+    .replace(`<!--app-head-->`, rendered.head ?? "")
+    .replace(`<!--app-html-->`, rendered.html ?? "")
+    .replace(
+      `<!-- app-data -->`,
+      `<script>window.__INITIAL_DATA__ = ${JSON.stringify(rendered.__INITIAL_DATA__)};</script>`,
+    );
+
+  if (!fs.existsSync(parsedPath.dir)) {
+    fs.mkdirSync(parsedPath.dir, { recursive: true });
+  }
+
+  fs.writeFileSync(fullPathname, html);
 }
 
-// 실행
-generateStaticSite();
+const getProducts = ({ category2 } = {}) => {
+  const filterByCategory = (data) => {
+    if (category2) {
+      return data.filter((v) => v.category2 === category2);
+    }
+    return data;
+  };
+
+  const products = filterByCategory(items);
+
+  return { products: products.sort((a, b) => +a.lprice - +b.lprice).slice(0, 20), totalCount: products.length };
+};
+
+// 404 생성
+await generateStaticSite("/404.html");
+
+// 홈 생성
+await generateStaticSite("/");
+
+// 상세페이지 생성
+const { products } = getProducts();
+for (const { productId } of products) {
+  await generateStaticSite(`/product/${productId}/`);
+}
+
+vite.close();
