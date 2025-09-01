f02a712872ebd5a1529558b16d9cc36cbadb8972
chan9yu <dev.cgyeo@gmail.com>
2025-09-01 17:33:23 +0900

    feat: Vite 개발 모드와 프로덕션 SSR 환경 통합

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,24 +1,63 @@
-import compression from "compression";
 import express from "express";
 import * as fs from "fs";
-import sirv from "sirv";
 
+const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
-const base = process.env.BASE || "/front_6th_chapter4-1/vanilla/";
-
-const render = () => {
-  const html = fs.readFileSync("./dist/vanilla/index.html", "utf-8");
-  return html.replace("<!--app-html-->", /* HTML */ `<div>SSR 테스트</div>`);
-};
+const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "");
 
 const app = express();
 
-app.use(compression());
-app.use(base, sirv("./dist/vanilla", { extensions: [] }));
+const templateHtml = prod ? fs.readFileSync("./dist/vanilla/index.html", "utf-8") : "";
 
-app.get("*all", async (_, res) => {
-  const html = render();
-  res.status(200).set({ "Content-Type": "text/html" }).send(html);
+/** @type {import('vite').ViteDevServer | undefined} */
+let vite;
+
+if (!prod) {
+  const { createServer } = await import("vite");
+
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+    base,
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import("compression")).default;
+  const sirv = (await import("sirv")).default;
+
+  app.use(compression());
+  app.use(base, sirv("./dist/vanilla", { extensions: [] }));
+}
+
+app.use("*all", async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, "");
+
+    /** @type {string} */
+    let template;
+    /** @type {import('./src/main-server.js').render} */
+    let render;
+
+    if (!prod) {
+      template = fs.readFileSync("./index.html", "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule("/src/main-server.js")).render;
+    } else {
+      template = templateHtml;
+      render = (await import("./dist/vanilla-ssr/main-server.js")).render;
+    }
+
+    const rendered = await render(url, req.query);
+    const html = template
+      .replace(`<!--app-head-->`, rendered.head ?? "")
+      .replace(`<!--app-html-->`, rendered.html ?? "");
+
+    res.status(200).set({ "Content-Type": "text/html" }).send(html);
+  } catch (e) {
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
+  }
 });
 
 // Start http server
