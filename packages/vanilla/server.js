38c16000fa18ea58d3ff6efe774df90a14a31c2c
Amelia-Shin <gmldnjs0402@naver.com>
2025-09-01 23:25:17 +0900

    feat: Vite 통합 및 production 환경 처리를 통해 서버 측 렌더링 구현

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,31 +1,69 @@
+import fs from "node:fs/promises";
 import express from "express";
 
 const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
 const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "/");
 
+// Cached production assets
+const templateHtml = prod ? await fs.readFile("./dist/vanilla/index.html", "utf-8") : "";
+
 const app = express();
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+// Add Vite or respective production middlewares
+/** @type {import('vite').ViteDevServer | undefined} */
+let vite;
+if (!prod) {
+  const { createServer } = await import("vite");
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+    base,
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import("compression")).default;
+  const sirv = (await import("sirv")).default;
+  app.use(compression());
+  app.use(base, sirv("./dist/vanilla", { extensions: [] }));
+}
 
-app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+const { server } = await vite.ssrLoadModule("/src/mocks/serverBrowser.js");
+server.listen();
+
+// use , get 차이점
+// use 는 모든 요청에 대해 실행되고, get 은 특정 요청에 대해 실행됩니다.
+app.use("*all", async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, "");
+    console.log(req.query);
+    /** @type {string} */
+    let template;
+    /** @type {import('./src/main-server.js').render} */
+    let render;
+    if (!prod) {
+      // Always read fresh template in development
+      template = await fs.readFile("./index.html", "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule("/src/main-server.js")).render;
+    } else {
+      template = templateHtml;
+      render = (await import("./dist/vanilla-ssr/main-server.js")).render;
+    }
+
+    // server에 있는 값을 전달해주기 위해서 인자로 넘겨줘야함.
+    const rendered = await render(url, req.query);
+
+    const html = template
+      .replace(`<!--app-head-->`, rendered.head ?? "")
+      .replace(`<!--app-html-->`, rendered.html ?? "");
+
+    res.status(200).set({ "Content-Type": "text/html" }).send(html);
+  } catch (e) {
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
+  }
 });
 
 // Start http server
