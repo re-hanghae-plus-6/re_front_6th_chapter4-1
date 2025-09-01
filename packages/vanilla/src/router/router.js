076548da34d39d9e8b73645e3d4f0eb59a92b82b
YangS1s <sungjinahim@gmail.com>
2025-09-01 23:43:59 +0900

    fix:서버 라우팅 분기처리

--- packages/vanilla/src/router/router.js
+++ packages/vanilla/src/router/router.js
@@ -1,5 +1,6 @@
 // 글로벌 라우터 인스턴스
-import { Router } from "../lib";
+import { Router, ServerRouter } from "../lib";
 import { BASE_URL } from "../constants.js";
+import { isServer } from "../utils/isServer.js";
 
-export const router = new Router(BASE_URL);
+export const router = !isServer ? new Router(BASE_URL) : new ServerRouter();
