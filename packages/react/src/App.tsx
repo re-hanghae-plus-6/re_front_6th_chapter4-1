40e6128133f647f00d846acd678bbad89efb33e2
JiHoon-0330 <ljihoon0330@gmail.com>
2025-09-02 15:26:09 +0900

    페이지 ssr 설정 추가

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,28 +1,40 @@
-import { router, useCurrentPage } from "./router";
-import { HomePage, NotFoundPage, ProductDetailPage } from "./pages";
-import { useLoadCartStore } from "./entities";
+import type { StringRecord } from "@hanghae-plus/lib";
+import { useState } from "react";
 import { ModalProvider, ToastProvider } from "./components";
-
-// 홈 페이지 (상품 목록)
-router.addRoute("/", HomePage);
-router.addRoute("/product/:id/", ProductDetailPage);
-router.addRoute(".*", NotFoundPage);
+import { useLoadCartStore } from "./entities";
+import { router, useCurrentPage } from "./router";
+import { isServer } from "./utils";
 
 const CartInitializer = () => {
   useLoadCartStore();
   return null;
 };
 
+interface Props {
+  data: unknown;
+  query: StringRecord;
+}
+
 /**
  * 전체 애플리케이션 렌더링
  */
-export const App = () => {
+export const App = ({ data, query }: Props) => {
+  useState(() => {
+    if (isServer) {
+      router.query = query;
+    }
+  });
   const PageComponent = useCurrentPage();
 
   return (
     <>
       <ToastProvider>
-        <ModalProvider>{PageComponent ? <PageComponent /> : null}</ModalProvider>
+        <ModalProvider>
+          {PageComponent ? (
+            // @ts-expect-error initialData is unknowns
+            <PageComponent data={data} />
+          ) : null}
+        </ModalProvider>
       </ToastProvider>
       <CartInitializer />
     </>
--- packages/react/src/pages/HomePage.tsx
+++ packages/react/src/pages/HomePage.tsx
@@ -1,5 +1,18 @@
-import { useEffect } from "react";
-import { loadNextProducts, loadProductsAndCategories, ProductList, SearchBar } from "../entities";
+import { useEffect, useState } from "react";
+import { getCategories, getProducts } from "../api/productApi";
+import {
+  initialProductState,
+  loadNextProducts,
+  loadProductsAndCategories,
+  PRODUCT_ACTIONS,
+  ProductList,
+  productStore,
+  SearchBar,
+  type Categories,
+  type Product,
+} from "../entities";
+import { withServer } from "../router/withServer";
+import { isServer } from "../utils";
 import { PageWrapper } from "./PageWrapper";
 
 const headerLeft = (
@@ -26,23 +39,70 @@ const unregisterScrollHandler = () => {
   scrollHandlerRegistered = false;
 };
 
-export const HomePage = () => {
-  useEffect(() => {
-    registerScrollHandler();
-    loadProductsAndCategories();
+interface ServerResponse {
+  products: Product[];
+  categories: Categories;
+  totalCount: number;
+  loading: boolean;
+  status: string;
+}
 
-    return unregisterScrollHandler;
-  }, []);
+export const HomePage = withServer(
+  {
+    ssr: async ({ query }): Promise<ServerResponse> => {
+      const [
+        {
+          products,
+          pagination: { total },
+        },
+        categories,
+      ] = await Promise.all([getProducts(query), getCategories()]);
 
-  return (
-    <PageWrapper headerLeft={headerLeft}>
-      {/* 검색 및 필터 */}
-      <SearchBar />
+      return {
+        products,
+        categories,
+        totalCount: total,
+        loading: false,
+        status: "done",
+      };
+    },
+    metadata: async () => {
+      return {
+        title: "쇼핑몰 - 홈",
+      };
+    },
+  },
+  ({ data }: { data: ServerResponse }) => {
+    useState(() => {
+      if (isServer) {
+        productStore.dispatch({
+          type: PRODUCT_ACTIONS.SETUP,
+          payload: initialProductState,
+        });
+      }
+      productStore.dispatch({
+        type: PRODUCT_ACTIONS.SETUP,
+        payload: data,
+      });
+    });
 
-      {/* 상품 목록 */}
-      <div className="mb-6">
-        <ProductList />
-      </div>
-    </PageWrapper>
-  );
-};
+    useEffect(() => {
+      registerScrollHandler();
+      loadProductsAndCategories();
+
+      return unregisterScrollHandler;
+    }, []);
+
+    return (
+      <PageWrapper headerLeft={headerLeft}>
+        {/* 검색 및 필터 */}
+        <SearchBar />
+
+        {/* 상품 목록 */}
+        <div className="mb-6">
+          <ProductList />
+        </div>
+      </PageWrapper>
+    );
+  },
+);
