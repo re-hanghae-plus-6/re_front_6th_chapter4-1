e93a5007e63eb7906258c1ff89348223e0cf6e75
JiHoon-0330 <ljihoon0330@gmail.com>
2025-09-02 15:25:37 +0900

    라우터 수정

--- packages/react/src/router/router.ts
+++ packages/react/src/router/router.ts
@@ -1,6 +1,17 @@
 // 글로벌 라우터 인스턴스
 import { Router } from "@hanghae-plus/lib";
+import type { FC } from "react";
 import { BASE_URL } from "../constants";
-import type { FunctionComponent } from "react";
+import { HomePage, NotFoundPage, ProductDetailPage } from "../pages";
 
-export const router = new Router<FunctionComponent>(BASE_URL);
+export const router = new Router<FC<unknown>>(BASE_URL);
+
+const routes = {
+  "/": HomePage,
+  "/product/:id/": ProductDetailPage,
+  ".*": NotFoundPage,
+};
+
+Object.entries(routes).forEach(([path, page]) => {
+  router.addRoute(path, page);
+});
