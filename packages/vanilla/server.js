633aad2fb412b142bee58522dd7c4b869098364b
김지혜 <ria9810@gmail.com>
2025-09-02 23:46:26 +0900

    feat: SSR 서버 렌더링 기능 구현 및 테스트 통과
    - 서버용 라우터 및 데이터 프리페칭 구현
    - HTML 템플릿 치환 및 초기 데이터 주입

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,31 +1,66 @@
+import fs from "node:fs/promises";
 import express from "express";
 
 const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
 const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "/");
 
+const templateHtml = prod ? await fs.readFile("./dist/client/index.html", "utf-8") : "";
+
 const app = express();
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+let vite;
+// use 는 모든 요청에 대해 실행되고, get 은 특정 요청에 대해 실행
+if (!prod) {
+  const { createServer } = await import("vite");
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+    base,
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import("compression")).default;
+  const sirv = (await import("sirv")).default;
+  app.use(compression());
+  app.use(base, sirv("./dist/client", { extensions: [] }));
+}
+app.use("*all", async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, "");
 
-app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+    /** @type {string} */
+    let template;
+    /** @type {import('./src/main-server.js').render} */
+    let render;
+    if (!prod) {
+      // Always read fresh template in development
+      template = await fs.readFile("./index.html", "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule("/src/main-server.js")).render;
+    } else {
+      template = templateHtml;
+      render = (await import("./dist/vanilla-ssr/main-server.js")).render;
+    }
+
+    // server에 있는 값을 전달해주기 위해서 인자로 넘겨줘야함.
+    const query = req.query;
+    const rendered = await render(url, query);
+
+    const html = template
+      .replace(`<!--app-head-->`, rendered.head ?? "")
+      .replace(`<!--app-html-->`, rendered.html ?? "")
+      .replace(
+        `<!--app-data-->`,
+        `<script>window.__INITIAL_DATA__ = ${JSON.stringify(rendered.initialData)};</script>`,
+      );
+
+    res.status(200).set({ "Content-Type": "text/html" }).send(html);
+  } catch (e) {
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
+  }
 });
 
 // Start http server
