311c62757499595d28992979fee1dda60ccc9fd7
JHeeJinDev <zqewe12@gmail.com>
2025-09-03 00:52:25 +0900

    feat: Express SSR 서버 및 서버 렌더링 기본 구조 구현

--- packages/vanilla/src/main-server.js
+++ packages/vanilla/src/main-server.js
@@ -1,4 +1,9 @@
+import { HomePage } from "./pages";
+
 export const render = async (url, query) => {
   console.log({ url, query });
-  return "";
+  return {
+    head: "<title>My App</title>",
+    html: HomePage(),
+  };
 };
