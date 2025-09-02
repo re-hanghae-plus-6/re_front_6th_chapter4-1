cabef6f6147396c55fe3b79ea80b151950756ebd
yuhyeon99 <jujini31@gmail.com>
2025-09-02 23:17:56 +0900

    feat(vanilla): Express SSR 서버 기본 구조 구현
    - `server.js`: 모든 요청을 처리하는 미들웨어를 도입하여, `index.html`을 템플릿으로 사용하도록 변경했습니다. 서버에서 렌더링된 HTML, Head, 초기 데이터를 템플릿에 삽입합니다.
    - `main-server.js`: `server.js`의 요구사항에 맞춰 `render` 함수가 `{ html, head, initialData }` 형태의 객체를 반환하도록 수정했습니다.

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,34 +1,40 @@
+import fs from "fs/promises";
 import express from "express";
+import { render } from "./src/main-server.js";
 
+// Constants
 const prod = process.env.NODE_ENV === "production";
-const port = process.env.PORT || 5173;
+const port = process.env.PORT || 5174; // dev:ssr 포트
 const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "/");
 
+// Create Express app
 const app = express();
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+// Get template
+const template = await fs.readFile("./index.html", "utf-8");
 
-app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+// Add middleware for all requests
+app.use("*", async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, "");
+
+    const { html: appHtml, head: appHead, initialData } = await render(url);
+
+    const initialDataScript = `<script>window.__INITIAL_DATA__ = ${JSON.stringify(initialData)}</script>`;
+
+    const html = template
+      .replace("<!--app-head-->", appHead || "")
+      .replace("<!--app-html-->", appHtml || "SSR Failed")
+      .replace("</head>", `${initialDataScript}</head>`);
+
+    res.status(200).set({ "Content-Type": "text/html" }).end(html);
+  } catch (e) {
+    console.error("SSR Error:", e);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Start http server
 app.listen(port, () => {
-  console.log(`React Server started at http://localhost:${port}`);
+  console.log(`Server started at http://localhost:${port}`);
 });
--- packages/vanilla/src/main-server.js
+++ packages/vanilla/src/main-server.js
@@ -1,4 +1,8 @@
 export const render = async (url, query) => {
   console.log({ url, query });
-  return "";
+  return {
+    html: `<h1>Page not found</h1>`,
+    head: `<title>404 Not Found</title>`,
+    initialData: {},
+  };
 };
