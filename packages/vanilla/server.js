d41ddcfe2ff372033bc3419cb172bcbe07c35396
bebusl <bebus1998@naver.com>
2025-09-04 01:33:58 +0900

    feat: ssr를 위한 server.js 작성

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,31 +1,63 @@
+import fs from "node:fs/promises";
 import express from "express";
 
 const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
+
+// gh-pages 배포 기준
 const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "/");
+
+const templateHtml = prod ? await fs.readFile("./dist/vanilla/index.html", "utf-8") : "";
 
 const app = express();
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+let vite;
+if (!prod) {
+  // 개발 모드일 때, hmr을 제공하기 위함
+  const { createServer } = await import("vite");
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+    base,
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import("compression")).default;
+  app.use(compression());
+  // 👇 express 내장 static 사용
+  app.use(base, express.static("./dist/vanilla-ssr", { extensions: [] }));
+}
 
-app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+// Serve HTML
+app.use("*all", async (req, res) => {
+  try {
+    const url = req.originalUrl.replace(base, "");
+
+    /** @type {string} */
+    let template;
+    let render;
+    if (!prod) {
+      // 실시간 index.html 반영
+      template = await fs.readFile("./index.html", "utf-8");
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule("/src/main-server.js")).render;
+    } else {
+      template = templateHtml;
+      render = (await import("./dist/vanilla-ssr/main-server.js")).render;
+    }
+
+    const rendered = await render(url);
+
+    const html = template
+      .replace(`<!--app-head-->`, rendered.head ?? "")
+      .replace(`<!--app-html-->`, rendered.html ?? "");
+
+    res.status(200).set({ "Content-Type": "text/html" }).send(html);
+  } catch (e) {
+    vite?.ssrFixStacktrace(e);
+    console.log(e.stack);
+    res.status(500).end(e.stack);
+  }
 });
 
 // Start http server
