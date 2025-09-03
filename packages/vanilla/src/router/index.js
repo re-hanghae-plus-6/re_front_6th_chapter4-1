1dd29f4a155c1ec2acc7d6a79f5eb2b82f12bbd7
chan9yu <dev.cgyeo@gmail.com>
2025-09-03 11:28:46 +0900

    feat: 서버사이드 렌더링과 클라이언트 하이드레이션 HOC 추가

--- packages/vanilla/src/router/index.js
+++ packages/vanilla/src/router/index.js
@@ -1,2 +1,3 @@
 export * from "./router";
 export * from "./withLifecycle";
+export * from "./withUniversal";
---
+++ packages/vanilla/src/router/withUniversal.js
@@ -0,0 +1,27 @@
+import { isServer } from "../utils";
+import { routerInstance } from "./router";
+
+/**
+ * 서버사이드 렌더링과 클라이언트 하이드레이션을 처리하는 HOC
+ *
+ * @param {function} PageComponent - 렌더링할 페이지 컴포넌트
+ * @returns {function} SSR 지원 페이지 컴포넌트
+ */
+export const withUniversal = (PageComponent) => {
+  const UniversalPageComponent = (serverProps) => {
+    if (isServer()) {
+      return PageComponent(serverProps);
+    }
+
+    const props = {
+      query: routerInstance.query,
+      data: window.__INITIAL_DATA__,
+    };
+
+    delete window.__INITIAL_DATA__;
+
+    return PageComponent(props);
+  };
+
+  return UniversalPageComponent;
+};
