5ad6f5763205942aa054b4f29df89411c063e940
tksemf7410 <tksemf7410@gmail.com>
2025-09-05 03:04:07 +0900

    feat(react): 서버에서도 API 호출할 수 있도록 대응

--- packages/react/src/api/productApi.ts
+++ packages/react/src/api/productApi.ts
@@ -1,6 +1,15 @@
 // 상품 목록 조회
 import type { Categories, Product } from "../entities";
 import type { StringRecord } from "../types.ts";
+import { isServer } from "../utils/ssrUtils.ts";
+
+const withBaseUrl = (url: string) => {
+  if (isServer()) {
+    return `http://localhost:${process.env.PORT ?? 5174}${url}`;
+  }
+
+  return `${url}`;
+};
 
 interface ProductsResponse {
   products: Product[];
@@ -33,19 +42,19 @@ export async function getProducts(params: StringRecord = {}): Promise<ProductsRe
     sort,
   });
 
-  const response = await fetch(`/api/products?${searchParams}`);
+  const response = await fetch(`${withBaseUrl(`/api/products?${searchParams}`)}`);
 
   return await response.json();
 }
 
 // 상품 상세 조회
 export async function getProduct(productId: string): Promise<Product> {
-  const response = await fetch(`/api/products/${productId}`);
+  const response = await fetch(`${withBaseUrl(`/api/products/${productId}`)}`);
   return await response.json();
 }
 
 // 카테고리 목록 조회
 export async function getCategories(): Promise<Categories> {
-  const response = await fetch("/api/categories");
+  const response = await fetch(`${withBaseUrl("/api/categories")}`);
   return await response.json();
 }
