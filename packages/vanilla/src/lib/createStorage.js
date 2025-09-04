373ecc1af7b2dbe50f895df83b3ed1bf41825dd8
tksemf7410 <tksemf7410@gmail.com>
2025-09-04 21:57:19 +0900

    feat: 서버에서 storage 동작되도록 대응

--- packages/vanilla/src/lib/createStorage.js
+++ packages/vanilla/src/lib/createStorage.js
@@ -1,10 +1,28 @@
+import { isServer } from "../utils/ssrUtils";
+
+const createMemoryStorage = () => {
+  const state = {};
+
+  return {
+    getItem: (key) => state[key],
+    setItem: (key, value) => {
+      state[key] = value;
+    },
+    removeItem: (key) => {
+      delete state[key];
+    },
+  };
+};
+
+const memoryStorage = createMemoryStorage();
+
 /**
  * 로컬스토리지 추상화 함수
  * @param {string} key - 스토리지 키
  * @param {Storage} storage - 기본값은 localStorage
  * @returns {Object} { get, set, reset }
  */
-export const createStorage = (key, storage = window.localStorage) => {
+export const createStorage = (key, storage = isServer() ? memoryStorage : window.localStorage) => {
   const get = () => {
     try {
       const item = storage.getItem(key);
