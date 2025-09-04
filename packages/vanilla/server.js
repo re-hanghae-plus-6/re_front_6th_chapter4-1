aeef2cbbf71d8dba6e055be08109aa15c404badb
sangsoo <wsckm1@gmail.com>
2025-09-05 01:59:43 +0900

    feat: Express 서버 HTML 템플릿 치환 방식으로 변경

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,31 +1,26 @@
 import express from "express";
+import fs from "fs";
 
-const prod = process.env.NODE_ENV === "production";
+// const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
-const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "/");
+// const base = process.env.BASE || (prod ? "/front_6th_chapter4-1/vanilla/" : "/");
+
+const base = "/front_6th_chapter4-1/vanilla/";
 
 const app = express();
 
 const render = () => {
-  return `<div>안녕하세요</div>`;
+  const html = fs.readFileSync("./dist/vanilla/index.html", "utf8");
+  return html.replace("<!--app-html-->", `<div>안뇽</div>`);
 };
 
+const compression = (await import("compression")).default;
+const sirv = (await import("sirv")).default;
+app.use(compression());
+app.use(base, sirv("./dist/vanilla", { extensions: [] }));
+
 app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+  res.send(render());
 });
 
 // Start http server
