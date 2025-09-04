6bf64c5daa308f0ee725e7b0bb01d15bcf020e3f
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-09-05 03:56:51 +0900

    fix : 오류 수정..

--- packages/react/src/router/router.ts
+++ packages/react/src/router/router.ts
@@ -4,6 +4,6 @@ import { BASE_URL } from "../constants";
 import type { FunctionComponent } from "react";
 
 export const router =
-  typeof window === "undefined"
+  typeof window !== "undefined"
     ? new Router<FunctionComponent>(BASE_URL)
     : new ServerRouter<FunctionComponent>(BASE_URL);
