a49b1a69ba7d54f101c42c51abb17175022116f0
김수현 <kimsuhyeon@gimsuhyeon-ui-MacBookPro.local>
2025-09-05 01:41:31 +0900

    feat : ServerRouter 생성

---
+++ packages/lib/src/ServerRouter.ts
@@ -0,0 +1,141 @@
+import { createObserver } from "./createObserver";
+import type { AnyFunction, StringRecord } from "./types";
+
+interface Route<Handler extends AnyFunction> {
+  regex: RegExp;
+  paramNames: string[];
+  handler: Handler;
+  params?: StringRecord;
+}
+
+type QueryPayload = Record<string, string | number | undefined>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export class ServerRouter<Handler extends (...args: any[]) => any> {
+  readonly #routes: Map<string, Route<Handler>>;
+  readonly #observer = createObserver();
+  readonly #baseUrl;
+  #currentQuery: StringRecord;
+  #route: null | (Route<Handler> & { params: StringRecord; path: string });
+
+  constructor(baseUrl = "") {
+    this.#routes = new Map();
+    this.#route = null;
+    this.#baseUrl = baseUrl.replace(/\/$/, "");
+    this.#currentQuery = {};
+  }
+
+  get baseUrl() {
+    return this.#currentQuery;
+  }
+  get query() {
+    return this.#currentQuery; // ← 이제 읽힘!
+  }
+
+  set query(newQuery: QueryPayload) {
+    const newUrl = ServerRouter.getUrl(newQuery, this.#baseUrl);
+    this.push(newUrl);
+  }
+
+  get params() {
+    return this.#route?.params ?? {};
+  }
+
+  get route() {
+    return this.#route;
+  }
+
+  get target() {
+    return this.#route?.handler;
+  }
+
+  readonly subscribe = this.#observer.subscribe;
+
+  addRoute(path: string, handler: Handler) {
+    // 경로 패턴을 정규식으로 변환
+    const paramNames: string[] = [];
+    const regexPath = path
+      .replace(/:\w+/g, (match) => {
+        paramNames.push(match.slice(1)); // ':id' -> 'id'
+        return "([^/]+)";
+      })
+      .replace(/\//g, "\\/");
+
+    const regex = new RegExp(`^${this.#baseUrl}${regexPath}$`);
+
+    this.#routes.set(path, {
+      regex,
+      paramNames,
+      handler,
+    });
+  }
+
+  #findRoute(url = "/", origin = "http://localhost") {
+    const { pathname } = new URL(url, origin);
+    for (const [routePath, route] of this.#routes) {
+      const match = pathname.match(route.regex);
+      if (match) {
+        // 매치된 파라미터들을 객체로 변환
+        const params: StringRecord = {};
+        route.paramNames.forEach((name, index) => {
+          params[name] = match[index + 1];
+        });
+
+        return {
+          ...route,
+          params,
+          path: routePath,
+        };
+      }
+    }
+    return null;
+  }
+
+  push(url = "/") {
+    try {
+      this.#route = this.#findRoute(url);
+    } catch (error) {
+      console.error("라우터 네비게이션 오류:", error);
+    }
+  }
+
+  start(url = "/", query = {}) {
+    this.#currentQuery = query;
+    this.#route = this.#findRoute(url);
+    this.#observer.notify();
+  }
+
+  static parseQuery = (search = window.location.search) => {
+    const params = new URLSearchParams(search);
+    const query: StringRecord = {};
+    for (const [key, value] of params) {
+      query[key] = value;
+    }
+    return query;
+  };
+
+  static stringifyQuery = (query: QueryPayload) => {
+    const params = new URLSearchParams();
+    for (const [key, value] of Object.entries(query)) {
+      if (value !== null && value !== undefined && value !== "") {
+        params.set(key, String(value));
+      }
+    }
+    return params.toString();
+  };
+
+  static getUrl = (newQuery: QueryPayload, baseUrl = "") => {
+    const currentQuery = ServerRouter.parseQuery();
+    const updatedQuery = { ...currentQuery, ...newQuery };
+
+    // 빈 값들 제거
+    Object.keys(updatedQuery).forEach((key) => {
+      if (updatedQuery[key] === null || updatedQuery[key] === undefined || updatedQuery[key] === "") {
+        delete updatedQuery[key];
+      }
+    });
+
+    const queryString = ServerRouter.stringifyQuery(updatedQuery);
+    return `${baseUrl}${window.location.pathname.replace(baseUrl, "")}${queryString ? `?${queryString}` : ""}`;
+  };
+}
