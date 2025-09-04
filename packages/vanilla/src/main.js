26f613e48f6dedceb2dca249e935ac0a96a11672
현지수 <hyunzsu@naver.com>
2025-09-04 20:48:42 +0900

    feat: 하이드레이션 구현 

--- packages/vanilla/src/main.js
+++ packages/vanilla/src/main.js
@@ -4,6 +4,8 @@ import { registerAllEvents } from "./events";
 import { loadCartFromStorage } from "./services";
 import { router } from "./router";
 import { BASE_URL } from "./constants.js";
+import { productStore } from "./stores/productStore.js";
+import { PRODUCT_ACTIONS } from "./stores/actionTypes.js";
 
 const enableMocking = () =>
   import("./mocks/browser.js").then(({ worker }) =>
@@ -15,7 +17,16 @@ const enableMocking = () =>
     }),
   );
 
+function hydrateFromServer() {
+  if (typeof window !== "undefined" && window.__INITIAL_DATA__) {
+    const data = window.__INITIAL_DATA__;
+    productStore.dispatch({ type: PRODUCT_ACTIONS.SETUP, payload: data });
+    delete window.__INITIAL_DATA__;
+  }
+}
+
 function main() {
+  hydrateFromServer();
   registerAllEvents();
   registerGlobalEvents();
   loadCartFromStorage();
@@ -23,7 +34,7 @@ function main() {
   router.start();
 }
 
-if (import.meta.env.MODE !== "test") {
+if (import.meta.env.DEV) {
   enableMocking().then(main);
 } else {
   main();
