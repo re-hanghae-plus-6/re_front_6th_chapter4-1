828913c0c735fb07d3b19fcbc34ab9b4f6618765
HYOJIN <hyo.khj@gmail.com>
2025-09-05 07:12:21 +0900

    fix: vite.config 파일 수정

--- packages/vanilla/vite.config.js
+++ packages/vanilla/vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
 
-const base = process.env.NODE_ENV === "production" ? "/vanilla/" : "";
+const base = process.env.NODE_ENV === "production" ? "/front_6th_chapter4-1/vanilla/" : "";
 
-export default defineConfig({ base });
+export default defineConfig({
+  base,
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
