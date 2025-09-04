5e029d9b096d58abf8f046bc34fb59fb163818b8
ldhldh07 <dhlim07@naver.com>
2025-09-04 15:40:12 +0900

    feat: getPages 정

--- packages/vanilla/static-site-generate.js
+++ packages/vanilla/static-site-generate.js
@@ -1,12 +1,11 @@
 import fs from "fs";
+import { mockGetProducts } from "./src/mocks/mockApi.js";
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
-
+const DIST_DIR = "../../dist/";
 async function generateStaticSite() {
   // HTML 템플릿 읽기
-  const template = fs.readFileSync("../../dist/vanilla/index.html", "utf-8");
+  const template = fs.readFileSync(`${DIST_DIR}vanilla/index.html`, "utf-8");
+  const { render } = await import("./src/main-server.js");
 
   // 어플리케이션 렌더링하기
   const appHtml = render();
@@ -18,3 +17,18 @@ async function generateStaticSite() {
 
 // 실행
 generateStaticSite();
+
+async function getPages() {
+  const { products } = await mockGetProducts({ page: 1, limit: 20, sort: "price_asc" });
+
+  return [
+    { url: "/", filePath: `${DIST_DIR}vanilla/index.html` },
+    { url: "/404", filePath: `${DIST_DIR}vanilla/404.html` },
+    ...products.map((p) => ({
+      url: `/product/${p.productId}/`,
+      filePath: `${DIST_DIR}vanilla/product/${p.productId}/index.html`,
+    })),
+  ];
+}
+
+void getPages;
