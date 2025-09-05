bbfab7509ae6d3841b827b6e8a5239a20e9c1707
chan9yu <dev.cgyeo@gmail.com>
2025-09-05 12:11:53 +0900

    feat: 기존 기본과제 서버 세팅 기반으로 심화과제 서버 구성

--- packages/react/src/main-server.tsx
+++ packages/react/src/main-server.tsx
@@ -1,4 +1,18 @@
-export const render = async (url: string, query: Record<string, string>) => {
-  console.log({ url, query });
-  return "";
-};
+import { renderToString } from "react-dom/server";
+
+// eslint-disable-next-line react-refresh/only-export-components
+function App() {
+  return <div>안녕하세요 쇼핑몰 테스트입니다.</div>;
+}
+
+export class SSRService {
+  async render(url: string, query: Record<string, string>) {
+    console.log({ url, query });
+
+    return {
+      head: /* HTML */ `<title>쇼핑몰 - 홈</title>`,
+      html: renderToString(<App />),
+      data: { name: "changyu" },
+    };
+  }
+}
