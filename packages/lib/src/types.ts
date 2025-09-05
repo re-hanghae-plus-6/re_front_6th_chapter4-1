3182f491b4ddd53c960b5e5232e13bed67d09c44
chan9yu <dev.cgyeo@gmail.com>
2025-09-05 14:28:48 +0900

    refactor: 서버 / 클라이언트 라우터 분리, 라우터 팩토리 함수를 통해 환경별로 인스턴스를 만들 수 있도록 개선

--- packages/lib/src/types.ts
+++ packages/lib/src/types.ts
@@ -1,5 +1,13 @@
+import type { ServerRouter, SPARouter } from "./router";
+
 export type StringRecord = Record<string, string>;
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type AnyFunction = (...args: any[]) => any;
 
 export type Selector<T, S = T> = (state: T) => S;
+
+export type RouterInstance<
+  T extends AnyFunction,
+  R extends typeof SPARouter<T> | typeof ServerRouter<T> = typeof SPARouter<T> | typeof ServerRouter<T>,
+> = InstanceType<R>;
