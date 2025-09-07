aee307e19f28a57927b4bb8a805804d9df3e061e
Legitgoons <cksxkr5193@naver.com>
2025-09-07 15:58:53 +0900

    feat: router.js 분기처리 구현

--- packages/vanilla/src/router/router.js
+++ packages/vanilla/src/router/router.js
@@ -1,5 +1,12 @@
 // 글로벌 라우터 인스턴스
-import { Router } from "../lib";
+import { ClientRouter, ServerRouter } from "../lib";
 import { BASE_URL } from "../constants.js";
+import { HomePage } from "../pages/HomePage.js";
+import { ProductDetailPage } from "../pages/ProductDetailPage.js";
+import { NotFoundPage } from "../pages/NotFoundPage.js";
 
-export const router = new Router(BASE_URL);
+export const router = typeof window === "undefined" ? new ServerRouter(BASE_URL) : new ClientRouter(BASE_URL);
+
+router.addRoute("/", HomePage);
+router.addRoute("/product/:id/", ProductDetailPage);
+router.addRoute(".*", NotFoundPage);
