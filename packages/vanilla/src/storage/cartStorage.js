17fa47295b7d77acef12a38da05da2eddaec2a9f
Legitgoons <cksxkr5193@naver.com>
2025-09-09 01:53:18 +0900

    feat: 실행 환경 구분 변경

--- packages/vanilla/src/storage/cartStorage.js
+++ packages/vanilla/src/storage/cartStorage.js
@@ -1,6 +1,4 @@
+import { isServer } from "../utils";
 import { createMemoryStorage, createStorage } from "../lib";
 
-export const cartStorage = createStorage(
-  "shopping_cart",
-  typeof window === "undefined" ? createMemoryStorage() : window.localStorage,
-);
+export const cartStorage = createStorage("shopping_cart", isServer ? createMemoryStorage() : window.localStorage);
