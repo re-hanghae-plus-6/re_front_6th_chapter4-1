edec18a2039fdd15d1308d0fd39fdfaed0dd9b00
Legitgoons <cksxkr5193@naver.com>
2025-09-09 01:27:18 +0900

    feat: 서버 환경 라우터 격리

--- packages/vanilla/src/main-server.js
+++ packages/vanilla/src/main-server.js
@@ -1,12 +1,17 @@
-import { router } from "./router";
+import { ServerRouter } from "./lib";
+import { routerMatches } from "./router/router";
 
 export const render = async (pathname, query) => {
   console.log({ pathname, query });
+  const router = new ServerRouter(routerMatches);
   router.start(pathname, query);
-  const data = (await router.target?.ssr({ pathname, query })) ?? {};
+  const params = { pathname, query, params: router.params };
+  const data = (await router.target?.ssr?.(params)) ?? {};
+  const metadata = await router.target?.metadata?.(params);
+
   return {
-    head: "",
-    html: router.target({ pathname, query, data }),
+    head: `<title>${metadata?.title ?? ""}</title>`,
+    html: router.target({ ...params, data }),
     __INITIAL_DATA__: data,
   };
 };
